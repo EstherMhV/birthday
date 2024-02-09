340df539d8edda1744b8bf8ae8e6e004302eb30e
Diece98 <esther.mehal@gmail.com>
2024-02-09 14:33:22 +0100

    svg1

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Quote from './Quote';
 import SvgImage from './Svg.js';
 import svgSrc from './image/happy-birthday.svg';
 
+
+
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="content">
         <div className="left-column">
-          <SvgImage src={svgSrc} />
-          {/* Add more SvgImage components as needed */}
+        <SvgImage src={svgSrc} />
+          {}
         </div>
         <div className="right-column">
           <Quote />
-          <SvgImage src={svgSrc} />
+          
         </div>
       </div>
     </div>
